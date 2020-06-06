293da9db5c8e767d969f9cc8d2ecde0d72d684e5
Tony Josi <tonyjosinew@gmail.com>
2020-06-07 00:25:23 +0530

    base header file added

---
+++ drivers/device_headers/stm32g0xx_device.h
@@ -0,0 +1,21 @@
+/**
+ *  @file   stm32g0xx_device.h
+ *  @brief  CMSIS Cortex-M0+ Device Peripheral Access Layer Header File.
+ *
+ *  This file contains all the peripheral register's definitions, bits
+ *  definitions and memory mapping for stm32g070xx devices.
+ *
+ *  @author         Tony Josi   https://tonyjosi97.github.io/profile/
+ *  @copyright      Copyright (C) 2020 Tony Josi
+ *  @bug            No known bugs.
+ */
+
+#ifndef __STM32G0XX_DEVICE_H
+#define __STM32G0XX_DEVICE_H
+
+#define FLASH_BASE_ADDRESS					(0x00000000UL)	/*!< FLASH base address */
+#define ROM_BASE_ADDRESS					(0x1FFF0000UL)	/*!< ROM base address */
+#define SRAM_BASE_ADDRESS					(0x20000000UL)	/*!< SRAM base address */
+
+
+#endif /* __STM32G0XX_DEVICE_H */
