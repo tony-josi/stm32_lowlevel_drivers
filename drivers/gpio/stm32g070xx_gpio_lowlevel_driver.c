4f5b366ec5173a60894a86756419e32805c7b70c
Tony Josi <tonyjosinew@gmail.com>
2020-06-07 13:32:07 +0530

    src & header files added for gpio drv file

---
+++ drivers/gpio/stm32g070xx_gpio_lowlevel_driver.c
@@ -0,0 +1,13 @@
+/**
+ *  @file   stm32g070xx_gpio_lowlevel_driver.c
+ *  @brief  GPIO Low level driver source file
+ *
+ *  This file contains the function definitions for low level
+ *  GPIO functions
+ *
+ *  @author         Tony Josi   https://tonyjosi97.github.io/profile/
+ *  @copyright      Copyright (C) 2020 Tony Josi
+ *  @bug            No known bugs.
+ */
+
+
---
+++ drivers/gpio/stm32g070xx_gpio_lowlevel_driver.h
@@ -0,0 +1,13 @@
+/**
+ *  @file   stm32g070xx_gpio_lowlevel_driver.h
+ *  @brief  GPIO Low level driver header file
+ *
+ *  This file contains the function declarations & structure declarations
+ *  for low level GPIO functions
+ *
+ *  @author         Tony Josi   https://tonyjosi97.github.io/profile/
+ *  @copyright      Copyright (C) 2020 Tony Josi
+ *  @bug            No known bugs.
+ */
+
+
