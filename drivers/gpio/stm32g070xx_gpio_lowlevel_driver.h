c2415bcd48affc4fa5d590a5bcfad2a0801738fd
Tony Josi <tonyjosinew@gmail.com>
2020-06-07 23:38:32 +0530

    register cfg macros added

--- drivers/gpio/stm32g070xx_gpio_lowlevel_driver.h
+++ drivers/gpio/stm32g070xx_gpio_lowlevel_driver.h
@@ -23,11 +23,11 @@
  */
 typedef struct __GPIO_InitConfig_t {
 
-	uint8_t              pin;
-	uint8_t              mode;
-	uint8_t              pullup_pulldown;
-	uint8_t              op_speed;
-	uint8_t              alternate_func;
+	uint8_t              pin;                                 /*!< GPIO Pin */
+	uint8_t              mode;                                /*!< GPIO mode  @GPIO_Register_CFG */
+	uint8_t              pullup_pulldown;                     /*!< GPIO Pull-up/Pull-down @GPIO_Register_CFG */
+	uint8_t              op_speed;                            /*!< GPIO Output speed @GPIO_Register_CFG */
+	uint8_t              alternate_func;                      /*!< GPIO Alternate function */
 
 } GPIO_InitConfig_t;
 
@@ -43,6 +43,43 @@ typedef struct __GPIO_Handle_t {
 	GPIO_InitConfig_t    GPIO_InitFields;                     /*!< GPIO initialization fields */
 
 } GPIO_Handle_t;
+
+/* GPIOs Register CFG Macros */
+/* ------------------------- */
+
+/**
+ * @addtogroup GPIO_Register_CFG
+ * @brief GPIOs Mode Register CFG Macros
+ */
+#define GPIO_MODE_IN                        (0x00000000u)   /*!< Input Floating Mode                   */
+#define GPIO_MODE_OUT_PP                    (0x00000001u)   /*!< Output Push Pull Mode                 */
+#define GPIO_MODE_OUT_OD                    (0x00000011u)   /*!< Output Open Drain Mode                */
+#define GPIO_MODE_AF_PP                     (0x00000002u)   /*!< Alternate Function Push Pull Mode     */
+#define GPIO_MODE_AF_OD                     (0x00000012u)   /*!< Alternate Function Open Drain Mode    */
+#define GPIO_MODE_ANALOG                    (0x00000003u)   /*!< Analog Mode  */
+#define GPIO_MODE_IT_RT                     (0x10110000u)   /*!< External Interrupt Mode with Rising edge trigger detection          */
+#define GPIO_MODE_IT_FT                     (0x10210000u)   /*!< External Interrupt Mode with Falling edge trigger detection         */
+#define GPIO_MODE_IT_RFT                    (0x10310000u)   /*!< External Interrupt Mode with Rising/Falling edge trigger detection  */
+#define GPIO_MODE_EVT_RT                    (0x10120000u)   /*!< External Event Mode with Rising edge trigger detection               */
+#define GPIO_MODE_EVT_FT                    (0x10220000u)   /*!< External Event Mode with Falling edge trigger detection              */
+#define GPIO_MODE_EVT_RFT                   (0x10320000u)   /*!< External Event Mode with Rising/Falling edge trigger detection       */
+
+/**
+ * @addtogroup GPIO_Register_CFG
+ * @brief GPIOs Output speed Register CFG Macros
+ */
+#define GPIO_SPEED_LOW                      (0x00000000u)  /*!< Low speed       */
+#define GPIO_SPEED_MEDIUM                   (0x00000001u)  /*!< Medium speed    */
+#define GPIO_SPEED_HIGH                     (0x00000002u)  /*!< High speed      */
+#define GPIO_SPEED_VERY_HIGH                (0x00000003u)  /*!< Very high speed */
+
+/**
+ * @addtogroup GPIO_Register_CFG
+ * @brief GPIOs Pull-up/Pull-down Register CFG Macros
+ */
+#define  GPIO_NOPULL                        (0x00000000u)   /*!< No Pull-up or Pull-down activation  */
+#define  GPIO_PULLUP                        (0x00000001u)   /*!< Pull-up activation                  */
+#define  GPIO_PULLDOWN                      (0x00000002u)   /*!< Pull-down activation                */
 
 /**
  * @brief GPIO Initialize
