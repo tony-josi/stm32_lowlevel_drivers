b09c2095c6a4eb16f8c38121abe2ac51ce124015
Tony Josi <tonyjosinew@gmail.com>
2020-06-08 23:47:36 +0530

    formatted

--- drivers/device_headers/stm32g0xx_device.h
+++ drivers/device_headers/stm32g0xx_device.h
@@ -19,17 +19,17 @@
 /* Core memory address */
 /* ------------------- */
 
-#define FLASH_BASE_ADDR                 (0x08000000UL)	/*!< FLASH base address */
-#define ROM_BASE_ADDR                   (0x1FFF0000UL)	/*!< ROM base address */
-#define SRAM_BASE_ADDR                  (0x20000000UL)	/*!< SRAM base address */
+#define FLASH_BASE_ADDR                 (0x08000000UL)  /*!< FLASH base address */
+#define ROM_BASE_ADDR                   (0x1FFF0000UL)  /*!< ROM base address */
+#define SRAM_BASE_ADDR                  (0x20000000UL)  /*!< SRAM base address */
 
 /* Peripheral Bus */
 /* -------------- */
 
-#define APB1_BASE_ADDR                  (0x40000000UL)	/*!< APB1 base address */
-#define APB2_BASE_ADDR                  (0x40010000UL)	/*!< APB1 base address */
-#define AHB_BASE_ADDR                   (0x40020000UL)	/*!< AHB base address */
-#define IOPORT_BUS_BASE_ADDR            (0x50000000UL)	/*!< IOPORT base address */
+#define APB1_BASE_ADDR                  (0x40000000UL)  /*!< APB1 base address */
+#define APB2_BASE_ADDR                  (0x40010000UL)  /*!< APB1 base address */
+#define AHB_BASE_ADDR                   (0x40020000UL)  /*!< AHB base address */
+#define IOPORT_BUS_BASE_ADDR            (0x50000000UL)  /*!< IOPORT base address */
 
 /* AHB Peripherals */
 /* --------------- */
@@ -77,11 +77,11 @@
 /* IOPORT_BUS Peripherals, GPIOs */
 /* ----------------------------- */
 
-#define GPIOA_BASE_ADDR                 (IOPORT_BUS_BASE_ADDR + 0x00000000UL)	/*!< GPIOA base address */
-#define GPIOB_BASE_ADDR                 (IOPORT_BUS_BASE_ADDR + 0x00000400UL)	/*!< GPIOB base address */
-#define GPIOC_BASE_ADDR                 (IOPORT_BUS_BASE_ADDR + 0x00000800UL)	/*!< GPIOC base address */
-#define GPIOD_BASE_ADDR                 (IOPORT_BUS_BASE_ADDR + 0x00000C00UL)	/*!< GPIOD base address */
-#define GPIOF_BASE_ADDR                 (IOPORT_BUS_BASE_ADDR + 0x00001400UL)	/*!< GPIOF base address */
+#define GPIOA_BASE_ADDR                 (IOPORT_BUS_BASE_ADDR + 0x00000000UL) /*!< GPIOA base address */
+#define GPIOB_BASE_ADDR                 (IOPORT_BUS_BASE_ADDR + 0x00000400UL) /*!< GPIOB base address */
+#define GPIOC_BASE_ADDR                 (IOPORT_BUS_BASE_ADDR + 0x00000800UL) /*!< GPIOC base address */
+#define GPIOD_BASE_ADDR                 (IOPORT_BUS_BASE_ADDR + 0x00000C00UL) /*!< GPIOD base address */
+#define GPIOF_BASE_ADDR                 (IOPORT_BUS_BASE_ADDR + 0x00001400UL) /*!< GPIOF base address */
 
 
 /* Peripheral register structures */
