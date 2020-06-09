93584840db54d3abe381f289c61ecbfd663ac9a6
Tony Josi <tonyjosinew@gmail.com>
2020-06-09 12:03:51 +0530

    inc included

---
+++ Inc/inc_placeholder.h
@@ -0,0 +1,15 @@
+/**
+ *  @file   inc_placeholder.h
+ *  @brief  Initial header file for holding inc folder
+ *
+ *  @author         Tony Josi   https://tonyjosi97.github.io/profile/
+ *  @copyright      Copyright (C) 2020 Tony Josi
+ *  @bug            No known bugs.
+ */
+
+#ifndef INC_PLACEHOLDER_H_
+#define INC_PLACEHOLDER_H_
+
+
+
+#endif /* INC_PLACEHOLDER_H_ */
