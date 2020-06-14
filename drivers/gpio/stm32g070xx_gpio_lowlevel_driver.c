31d22e42c784e44211771a098c28c520975efb1d
Tony Josi <tonyjosinew@gmail.com>
2020-06-14 22:58:04 +0530

    gpio init cleanup

--- drivers/gpio/stm32g070xx_gpio_lowlevel_driver.c
+++ drivers/gpio/stm32g070xx_gpio_lowlevel_driver.c
@@ -24,67 +24,63 @@ Drv_Status_t LL_HAL_GPIO_Init(GPIO_Handle_t *hGPIO) {
   drv_assert_param(hGPIO);
   uint32_t reg_buff = 0;
 
-  if(hGPIO->GPIO_InitFields.mode <= GPIO_MODE_ANALOG) {
+  if (hGPIO->GPIO_InitFields.mode <= GPIO_MODE_IT_RFT) {
 
     reg_buff = hGPIO->GPIO_regdef->MODER;
     reg_buff &= ~((3U) << (hGPIO->GPIO_InitFields.pin * 2));
     reg_buff |= ((hGPIO->GPIO_InitFields.mode) << (hGPIO->GPIO_InitFields.pin * 2));
     hGPIO->GPIO_regdef->MODER = reg_buff;
 
-    if(hGPIO->GPIO_InitFields.mode == GPIO_MODE_AF) {
+    if(hGPIO->GPIO_InitFields.mode <= GPIO_MODE_ANALOG) {
 
-      if(hGPIO->GPIO_InitFields.alternate_func_mode <= GPIO_AF_7) {
+      if(hGPIO->GPIO_InitFields.mode == GPIO_MODE_AF) {
 
-        uint8_t AFR_index, AFR_AF_bits;
-        AFR_index = hGPIO->GPIO_InitFields.pin / 8;
-        AFR_AF_bits = hGPIO->GPIO_InitFields.pin % 8;
+        if(hGPIO->GPIO_InitFields.alternate_func_mode <= GPIO_AF_7) {
 
-        reg_buff = hGPIO->GPIO_regdef->AFR[AFR_index];
-        reg_buff &= ~((0xFU) << (AFR_AF_bits * 4));
-        reg_buff |= ((hGPIO->GPIO_InitFields.alternate_func_mode) << (AFR_AF_bits * 4));
-        hGPIO->GPIO_regdef->AFR[AFR_index] = reg_buff;
+          uint8_t AFR_index, AFR_AF_bits;
+          AFR_index = hGPIO->GPIO_InitFields.pin / 8;
+          AFR_AF_bits = hGPIO->GPIO_InitFields.pin % 8;
 
-      } else
-        return DRV_ERROR;
+          reg_buff = hGPIO->GPIO_regdef->AFR[AFR_index];
+          reg_buff &= ~((0xFU) << (AFR_AF_bits * 4));
+          reg_buff |= ((hGPIO->GPIO_InitFields.alternate_func_mode) << (AFR_AF_bits * 4));
+          hGPIO->GPIO_regdef->AFR[AFR_index] = reg_buff;
+
+        } else
+          return DRV_ERROR;
+      }
+
+    } else {
+
+      if (hGPIO->GPIO_InitFields.mode == GPIO_MODE_IT_RT) {
+
+        EXTI->RTSR1 |= (1 << hGPIO->GPIO_InitFields.pin);
+        EXTI->FTSR1 &= ~(1 << hGPIO->GPIO_InitFields.pin);
+
+      } else if (hGPIO->GPIO_InitFields.mode == GPIO_MODE_IT_FT) {
+
+        EXTI->FTSR1 |= (1 << hGPIO->GPIO_InitFields.pin);
+        EXTI->RTSR1 &= ~(1 << hGPIO->GPIO_InitFields.pin);
+
+      } else if (hGPIO->GPIO_InitFields.mode == GPIO_MODE_IT_RFT) {
+
+        EXTI->FTSR1 |= (1 << hGPIO->GPIO_InitFields.pin);
+        EXTI->RTSR1 |= (1 << hGPIO->GPIO_InitFields.pin);
+      }
+
+      /* Configure EXTI external interrupt selection register */
+      uint8_t temp_EXTI_EXTICRx_idx = hGPIO->GPIO_InitFields.pin / EXTI_EXTICR_REG_COUNT;
+      uint8_t temp_EXTI_EXTICRx_shftr = (hGPIO->GPIO_InitFields.pin % 4) * EXTI_EXTICR_EXTI_SEL_WIDTH;
+      uint8_t port_code = GPIO_PORT_TO_CODE(hGPIO->GPIO_regdef);
+
+      EXTI->EXTICR[temp_EXTI_EXTICRx_idx] |= port_code << temp_EXTI_EXTICRx_shftr;
+
+      /* Enable the Interrupt Mask Register */
+      EXTI->IMR1 |= (1 << hGPIO->GPIO_InitFields.pin);
     }
 
-  } else if (hGPIO->GPIO_InitFields.mode <= GPIO_MODE_IT_RFT) {
-
-    reg_buff = hGPIO->GPIO_regdef->MODER;
-    reg_buff &= ~((3U) << (hGPIO->GPIO_InitFields.pin * 2));
-    reg_buff |= ((hGPIO->GPIO_InitFields.mode) << (hGPIO->GPIO_InitFields.pin * 2));
-    hGPIO->GPIO_regdef->MODER = reg_buff;
-
-    if (hGPIO->GPIO_InitFields.mode == GPIO_MODE_IT_RT) {
-
-      EXTI->RTSR1 |= (1 << hGPIO->GPIO_InitFields.pin);
-      EXTI->FTSR1 &= ~(1 << hGPIO->GPIO_InitFields.pin);
-
-    } else if (hGPIO->GPIO_InitFields.mode == GPIO_MODE_IT_FT) {
-
-      EXTI->FTSR1 |= (1 << hGPIO->GPIO_InitFields.pin);
-      EXTI->RTSR1 &= ~(1 << hGPIO->GPIO_InitFields.pin);
-
-    } else if (hGPIO->GPIO_InitFields.mode == GPIO_MODE_IT_RFT) {
-
-      EXTI->FTSR1 |= (1 << hGPIO->GPIO_InitFields.pin);
-      EXTI->RTSR1 |= (1 << hGPIO->GPIO_InitFields.pin);
-    }
-
-    /* Configure EXTI external interrupt selection register */
-    uint8_t temp_EXTI_EXTICRx_idx = hGPIO->GPIO_InitFields.pin / EXTI_EXTICR_REG_COUNT;
-    uint8_t temp_EXTI_EXTICRx_shftr = (hGPIO->GPIO_InitFields.pin % 4) * EXTI_EXTICR_EXTI_SEL_WIDTH;
-    uint8_t port_code = GPIO_PORT_TO_CODE(hGPIO->GPIO_regdef);
-
-    EXTI->EXTICR[temp_EXTI_EXTICRx_idx] |= port_code << temp_EXTI_EXTICRx_shftr;
-
-    /* Enable the Interrupt Mask Register */
-    EXTI->IMR1 |= (1 << hGPIO->GPIO_InitFields.pin);
-
-  } else {
-
+  } else
     return DRV_ERROR;
-  }
 
   if(hGPIO->GPIO_InitFields.op_speed <= GPIO_SPEED_VERY_HIGH) {
 
