0b5e7adea429e02fe0d99f053b109fde41803379
Tony Josi <tonyjosinew@gmail.com>
2020-06-19 00:37:43 +0530

    SPI SPI_RegDef_Type

--- drivers/device_headers/stm32g0xx_device.h
+++ drivers/device_headers/stm32g0xx_device.h
@@ -213,6 +213,23 @@ typedef struct {
 
 } EXTI_RegDef_Type;
 
+/**
+  * @brief Serial Peripheral Interface
+  */
+typedef struct {
+
+  __VL uint32_t CR1;            /*!< SPI Control register 1 (not used in I2S mode),       Address offset: 0x00 */
+  __VL uint32_t CR2;            /*!< SPI Control register 2,                              Address offset: 0x04 */
+  __VL uint32_t SR;             /*!< SPI Status register,                                 Address offset: 0x08 */
+  __VL uint32_t DR;             /*!< SPI data register,                                   Address offset: 0x0C */
+  __VL uint32_t CRCPR;          /*!< SPI CRC polynomial register (not used in I2S mode),  Address offset: 0x10 */
+  __VL uint32_t RXCRCR;         /*!< SPI Rx CRC register (not used in I2S mode),          Address offset: 0x14 */
+  __VL uint32_t TXCRCR;         /*!< SPI Tx CRC register (not used in I2S mode),          Address offset: 0x18 */
+  __VL uint32_t I2SCFGR;        /*!< SPI_I2S configuration register,                      Address offset: 0x1C */
+  __VL uint32_t I2SPR;          /*!< SPI_I2S prescaler register,                          Address offset: 0x20 */
+
+} SPI_RegDef_Type;
+
 
 /**
   * @brief System configuration controller
