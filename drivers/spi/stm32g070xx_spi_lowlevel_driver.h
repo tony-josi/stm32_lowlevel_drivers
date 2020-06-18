55bbe830076ffaea0bb755b377a4b23ec7d9fda0
Tony Josi <tonyjosinew@gmail.com>
2020-06-19 00:27:52 +0530

    SPI files added

---
+++ drivers/spi/stm32g070xx_spi_lowlevel_driver.h
@@ -0,0 +1,19 @@
+/**
+ *  @file   stm32g070xx_spi_lowlevel_driver.c
+ *  @brief  Serial Peripheral Interface
+ *          Low level driver header file
+ *
+ *  This file contains the function declarations & structure declarations
+ *  for low level SPI functions. The Serial Peripheral Interface is a synchronous
+ *  serial communication interface specification used for
+ *  short-distance communication, primarily in embedded systems.
+ *
+ *  @author         Tony Josi   https://tonyjosi97.github.io/profile/
+ *  @copyright      Copyright (C) 2020 Tony Josi
+ *  @bug            No known bugs.
+ */
+
+#ifndef __STM32070XX_SPI_LOWLEVEL_DRIVER_H
+#define __STM32070XX_SPI_LOWLEVEL_DRIVER_H
+
+#endif /* __STM32070XX_SPI_LOWLEVEL_DRIVER_H */
