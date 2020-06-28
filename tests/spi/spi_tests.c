4e3c2e8373cbd5e740c95fc04bd775d4d797eece
Tony Josi <tonyjosinew@gmail.com>
2020-06-29 00:02:29 +0530

    SPI test file added

---
+++ tests/spi/spi_tests.c
@@ -0,0 +1,10 @@
+/**
+ *  @file   spi_test.c
+ *  @brief  Source file for GPIO test cases
+ *
+ *  This file contains SPI tests.
+ *
+ *  @author         Tony Josi   https://tonyjosi97.github.io/profile/
+ *  @copyright      Copyright (C) 2020 Tony Josi
+ *  @bug            No known bugs.
+ */
